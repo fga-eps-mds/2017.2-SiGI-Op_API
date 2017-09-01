ba9065bf69b471db05cbf2fef64b91c12164cd33
Mateus Andrade <mateusandrade080@gmail.com>
2017-08-31 22:12:02 -0300

    Add CI and repo settings

--- sigi_op/settings.py
+++ sigi_op/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+    '--cover-package=ipa'
 ]
 
 MIDDLEWARE = [
