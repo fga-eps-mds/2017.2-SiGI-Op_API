01190914c2010ff8548b958efc0345dc782d870c
Filipe Toyoshima <filipetoyoshima@gmail.com>
2017-09-17 18:42:43 -0300

    Final adjustments do flake8

--- dgo/migrations/0001_initial.py
+++ dgo/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='GOD',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True,
-            serialize=False, verbose_name='ID')),
+                                        serialize=False, verbose_name='ID')),
                 ('code', models.IntegerField(default=0)),
                 ('fabricant', models.CharField(max_length=50)),
                 ('port_quantity', models.IntegerField(default=1)),
