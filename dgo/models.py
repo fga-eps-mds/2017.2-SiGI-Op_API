3c7d559c43e5cd23210f710bbb87985aa82968a8
Filipe Toyoshima <filipetoyoshima@gmail.com>
2017-09-17 16:38:25 -0300

    Added class GOD

--- dgo/models.py
+++ dgo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class GOD(models.Model):
+    code = models.IntegerField(default=0)
+    fabricant = models.CharField(max_length=50)
+    port_quantity = models.IntegerField(default=1)
