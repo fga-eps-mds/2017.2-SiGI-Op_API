2446e0006952ea5263f762418cc3d2fa49e87741
Filipe Toyoshima <filipetoyoshima@gmail.com>
2017-09-17 18:30:54 -0300

    Adjustments to flake8

--- dgo/views.py
+++ dgo/views.py
@@ -1,6 +1,6 @@
-#from django.shortcuts import get_object_or_404, render
-#from rest_framework import status, viewsets
-#from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404, render
+from rest_framework import viewsets
+# from rest_framework.response import Response
 from sigi_op.serializers import GODSerializer
 from .models import GOD
 
