0019790468bdbf9857c354ae7aaaeb83c17296f3
Eduardo Lima <eduardolimrib@gmail.com>
2017-09-21 22:14:30 -0300

    Created calls for site_detail in urls.

--- ipa/urls.py
+++ ipa/urls.py
@@ -1,12 +1,22 @@
 from .views import SiteListViewSet, SiteTypeListViewSet
+
 # from rest_framework import renderers
 
 site = SiteListViewSet.as_view({
     'get': 'list',
-    'post': 'create'
-    })
+    'post': 'create',
+})
+
+site_detail = SiteListViewSet.as_view({
+    'get': 'list',
+    'patch': 'partial_update',
+    'put': 'update',
+    'delete': 'destroy',
+})
 
 sitetype = SiteTypeListViewSet.as_view({
     'get': 'list',
-    'post': 'create'
+    'patch': 'partial_update',
+    'put': 'update',
+    'delete': 'destroy',
 })
