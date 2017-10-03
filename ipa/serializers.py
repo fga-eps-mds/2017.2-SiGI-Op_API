cc649dfde6f25bf347e88ba7389554bd75518467
Eduardo Lima <eduardolimrib@gmail.com>
2017-10-03 07:24:03 -0300

    Created serializers for switch model.

--- ipa/serializers.py
+++ ipa/serializers.py
@@ -1,4 +1,5 @@
 from .models import InstitutionType, ParticipantInstitution, ContactType
+from .models import Switch
 from .models import Contact
 from rest_framework import serializers
 
@@ -45,3 +46,17 @@ class ContactSerializer(serializers.ModelSerializer):
                     'contact_type',
                     'ipa_code',
                 ]
+
+
+class SwitchSerializer(serializer.ModelSerializer):
+
+
+    class Meta:
+        model = Switch
+        fields = [
+                'serial_number',
+                'fabricant',
+                'qtd_slots',
+                'patrimony_number',
+                'site_id',
+        ]
