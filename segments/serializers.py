735078bc747bd1b7c59e4e6f69a42f5e31577083
Andrew Lucas <anlugueso@gmail.com>
2017-10-12 11:26:21 -0300

    delete .pyc files and change serelizer to show id

--- segments/serializers.py
+++ segments/serializers.py
@@ -6,6 +6,7 @@ class Segments_serializer_class(serializers.ModelSerializer):
     class Meta:
         model = Segment
         fields = [
+            'id',
             'cable_length',
             'segment_number',
         ]
