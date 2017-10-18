df7ebbba2c078a56a732b35d133ff21b66d7fb2f
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2017-10-18 21:57:07 -0200

    Refactoring undergroundbox model

--- underground_box/models.py
+++ underground_box/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from django.utils import timezone
-
+# from ../technical_reserve/models import TechnicalReserve
+# from ../emendation_box/models import EmendationBox
+from emendation_box.models import EmendationBox
+from technical_reserve.models import TechnicalReserve
 
 class UndergroundBoxType(models.Model):
     name = models.CharField(blank=False, max_length=100)
 
 
 class UndergroundBox(models.Model):
+    code = models.CharField(max_length=200, blank=False, default='none')
     box_type = models.ForeignKey(UndergroundBoxType, null=False)
     latitude = models.FloatField(blank=False)
-    cover_type = models.CharField(max_length=100, blank=False)
     longitude = models.FloatField(blank=False)
-    created_at = models.DateTimeField(default=timezone.now, blank=True)
-    removed_at = models.DateTimeField(default=timezone.now, blank=True)
-    draw_number = models.IntegerField(blank=False)
+    cover_type = models.CharField(max_length=20, blank=False)
+    emendation_box = models.ForeignKey(EmendationBox,null=False)
+    technical_reserve = models.ForeignKey(TechnicalReserve,null=False)
+    
--- underground_box/serializers.py
+++ underground_box/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import UndergroundBox, UndergroundBoxType
-
+# from ../emendation_box/serializers import EmendationBoxSerializer
+# from ../technical_reserve/serializers import TechnicalReserveSerializer
+from emendation_box.serializers import EmendationBoxSerializer
+from technical_reserve.serializers import TechnicalReserveSerializer
 
 class UndergroundBoxTypeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,13 +18,15 @@ class UndergroundBoxSerializer(serializers.ModelSerializer):
     class Meta:
         model = UndergroundBox
         box_type = UndergroundBoxTypeSerializer(many=True, read_only=True)
+        emendation_box = EmendationBoxSerializer(many=True, read_only=True)
+        technical_reserve = TechnicalReserveSerializer(many=True, read_only=True)
         fields = [
                 'id',
+                'code',
                 'box_type',
                 'latitude',
-                'cover_type',
                 'longitude',
-                'created_at',
-                'removed_at',
-                'draw_number',
+                'cover_type',
+                'emendation_box',
+                'technical_reserve',
         ]
