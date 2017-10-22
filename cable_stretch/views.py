22ad5035e2b06e50f1b464d4b3ca28df39eac6a5
Gabriel Alves <gabrielsoares.prog@gmail.com>
2017-10-22 17:46:47 -0200

    add views Tubeloose

--- cable_stretch/views.py
+++ cable_stretch/views.py
@@ -1,10 +1,17 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import CableStretchSerializer
+from .serializers import TubelooseSerializer
 from .models import CableStretch
+from .models import Tubeloose
 
 
 # Create your views here.
 class CableStretchListViewSet(viewsets.ModelViewSet):
     queryset = CableStretch.objects.all()
     serializer_class = CableStretchSerializer
+
+
+class TubelooseListViewSet(viewsets.ModelViewSet):
+    queryset = Tubeloose.objects.all()
+    serializer_class = TubelooseSerializer
