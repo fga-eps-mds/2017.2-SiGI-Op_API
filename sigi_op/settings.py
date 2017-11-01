29202a75e96fa03b596b5251f1ccd57c66c65244
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-11-01 19:52:10 -0200

    Including pagination on settings.py

--- sigi_op/settings.py
+++ sigi_op/settings.py
@@ -111,7 +111,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 25
 }
 
 import dj_database_url
