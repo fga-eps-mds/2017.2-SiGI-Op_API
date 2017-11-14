49b3fd85adacf5fd42992827c25e3a0cb2239d4c
filipetoyoshima <filipetoyoshima@gmail.com>
2017-11-14 11:00:42 -0200

    Flake8 Adjustments

--- cable_stretch/models.py
+++ cable_stretch/models.py
@@ -14,20 +14,25 @@ class CableStretchType(models.Model):
 class CableStretch(models.Model):
     cod = models.CharField(max_length=20)
     length = models.FloatField(blank=True, null=True)
-    manufacturing_year = models.IntegerField(blank=True,null=True,default=0)
-    infrastructure = models.CharField(blank=True,null=True,max_length=100)
-    owner = models.CharField(blank=True,null=True,max_length=100)
-    fabricant = models.CharField(blank=True,null=True,max_length=100)
-    cable_stretch_type = models.ForeignKey(CableStretchType,blank=True, null=True)
+    manufacturing_year = models.IntegerField(blank=True, null=True, default=0)
+    infrastructure = models.CharField(blank=True, null=True, max_length=100)
+    owner = models.CharField(blank=True, null=True, max_length=100)
+    fabricant = models.CharField(blank=True, null=True, max_length=100)
+    cable_stretch_type = models.ForeignKey(CableStretchType,
+                                           blank=True,
+                                           null=True)
     access = models.NullBooleanField(blank=True)
-    god_id = models.ForeignKey(GOD, null=True,blank=True)
-    segment_id = models.ForeignKey(Segments, null=True,blank=True)
-    access_cable_id = models.ForeignKey(AccessCable, null=True,blank=True)
-    creation_date = models.DateTimeField(null=True, auto_now_add=True,blank=True)
-    updated_date = models.DateTimeField(null=True, auto_now=True,blank=True)
+    god_id = models.ForeignKey(GOD, null=True, blank=True)
+    segment_id = models.ForeignKey(Segments, null=True, blank=True)
+    access_cable_id = models.ForeignKey(AccessCable, null=True, blank=True)
+    creation_date = models.DateTimeField(null=True,
+                                         auto_now_add=True,
+                                         blank=True)
+    updated_date = models.DateTimeField(null=True, auto_now=True, blank=True)
     # dgo some pk issue to solve
     # segment
     # access cable
+
     def save(self, *args, **kwargs):
         print("Debugging")
         ''' On save, update timestamps '''
