261d6893224f6396eb7f1a7eb9ee46f4dbb49741
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2017-11-14 23:13:36 -0200

    Fixed bug of not making queryset again

--- emendation_box/views.py
+++ emendation_box/views.py
@@ -7,15 +7,21 @@ from .models import EmendationBoxStructure, EmendationBoxType, EmendationBox
 
 # Create your views here.
 class EmendationBoxListViewSet(CustomViewSet):
+    class_name = EmendationBox
+    order_param_name = 'designNumber'
     queryset = EmendationBox.objects.all().order_by('designNumber')
     serializer_class = EmendationBoxSerializer
 
 
 class EmendationBoxTypeListViewSet(CustomViewSet):
+    class_name = EmendationBoxType
+    order_param_name = 'description'
     queryset = EmendationBoxType.objects.all().order_by('description')
     serializer_class = EmendationBoxTypeSerializer
 
 
 class EmendationBoxStructureListViewSet(CustomViewSet):
+    class_name = EmendationBoxStructure
+    order_param_name = 'description'
     queryset = EmendationBoxStructure.objects.all().order_by('description')
     serializer_class = EmendationBoxStructureSerializer
--- ipa/views.py
+++ ipa/views.py
@@ -22,70 +22,98 @@ from sigi_op.serializers import UserSerializer
 
 
 class IpaListViewSet(CustomViewSet):
+    class_name = ParticipantInstitution
+    order_param_name = 'name'
     queryset = ParticipantInstitution.objects.all().order_by('name')
     serializer_class = ParticipantInstitutionSerializer
 
 
 class IpaTypeListViewSet(CustomViewSet):
+    class_name = InstitutionType
+    order_param_name = 'description'
     queryset = InstitutionType.objects.all().order_by('description')
     serializer_class = ParticipantInstitutionTypeSerializer
 
 
 class SiteListViewSet(CustomViewSet):
+    class_name = Site
+    order_param_name = 'name'
     queryset = Site.objects.all().order_by('name')
     serializer_class = SiteSerializer
 
 
 class SiteTypeListViewSet(CustomViewSet):
+    class_name = SiteType
+    order_param_name = 'description'
     queryset = SiteType.objects.all().order_by('description')
     serializer_class = SiteTypeSerializer
 
 
 class ContactViewSet(CustomViewSet):
+    class_name = Contact
+    order_param_name = 'name'
     queryset = Contact.objects.all().order_by('name')
     serializer_class = ContactSerializer
 
 
 class ContactTypeViewSet(CustomViewSet):
+    class_name = ContactType
+    order_param_name = 'description'
     queryset = ContactType.objects.all().order_by('description')
     serializer_class = ContactTypeSerializer
 
 
 class GeneratorListViewSet(CustomViewSet):
+    class_name = Generator
+    order_param_name = 'site_id'
     queryset = Generator.objects.all().order_by('site_id')
     serializer_class = GeneratorSerializer
 
 
 class NoBreakViewSet(CustomViewSet):
+    class_name = NoBreak
+    order_param_name = 'site_id'
     queryset = NoBreak.objects.all().order_by('site_id')
     serializer_class = NoBreakSerializer
 
 
 class SwitchViewSet(CustomViewSet):
+    class_name = Switch
+    order_param_name = 'site_id'
     queryset = Switch.objects.all().order_by('site_id')
     serializer_class = SwitchSerializer
 
 
 class GroupViewSet(CustomViewSet):
+    class_name = Group
+    order_param_name = 'name'
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
 class PermissionViewSet(CustomViewSet):
+    class_name = Permission
+    order_param_name = 'codename'
     queryset = Permission.objects.all()
     serializer_class = PermissionSerializer
 
 
 class UserViewSet(CustomViewSet):
+    class_name = User
+    order_param_name = 'email'
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class SlotViewSet(CustomViewSet):
+    class_name = Slot
+    order_param_name = 'serie'
     queryset = Slot.objects.all().order_by('serie')
     serializer_class = SlotSerializer
 
 
 class SlotPortViewSet(CustomViewSet):
+    class_name = SlotPort
+    order_param_name = 'type'
     queryset = SlotPort.objects.all().order_by('type')
     serializer_class = SlotPortSerializer
